923e76a311f03b393d87b4d44b846e3bb6152110
Andrew Heim <dheim@tonicdesign.com>
2016-07-13 14:22:03 -0400

    add init and stubs

--- flirmebaby/FLIRDataSource.swift
+++ flirmebaby/FLIRDataSource.swift
@@ -1,1 +1,18 @@
 import Foundation
+class FLIRDataSource: NSObject, FLIROneSDKImageReceiverDelegate, FLIROneSDKStreamManagerDelegate {
+
+    override init() {
+        super.init()
+        FLIROneSDKStreamManager.sharedInstance().addDelegate(self)
+        FLIROneSDKSimulation.sharedInstance().connectWithFrameBundleName("sampleframes_hq", withBatteryChargePercentage: 42)
+    }
+
+    func FLIROneSDKDidConnect() {
+    }
+
+    func FLIROneSDKDelegateManager(delegateManager: NSObject!, didReceiveBlendedMSXRGBA8888Image msxImage: NSData!, imageSize size: CGSize) {
+    }
+
+    func FLIROneSDKDelegateManager(delegateManager: NSObject!, didReceiveRadiometricData radiometricData: NSData!, imageSize size: CGSize) {
+    }
+}
